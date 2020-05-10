0fbe9f8b865262d305f4408c6fdf3d4ca38390c5
theluispestana <theluispestana@gmail.com>
2020-05-10 14:11:06 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+const writeCards = (names, event) => {
+  let cards = [];
+  for (let i = 0; i < names.length; i++) {
+    cards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return cards;
+}
+
+const countDown = n => {
+  for (let i = n; i >= 0; i--) {
+    console.log(i);
+  }
+}
+
