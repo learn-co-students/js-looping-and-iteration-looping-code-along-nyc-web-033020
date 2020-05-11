ea3fc5e29d52d7c76dabf73784ac8ba555b0d6f8
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-05-10 23:02:25 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(names, event){
+  let messages = []
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+  }
+  return messages
+}
+
+function countDown(num){
+  let i = num;
+  while (i > -1) {
+    console.log(i);
+    i--
+  }
+}
